d0b2464f2ec461689fd9eee63154d25227692688
mohdsyahiran <mohamad.syahiran25@gmail.com>
2024-07-12 18:47:09 +0800

    Rename path styles

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendar Booking</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <div class="calendar-container">
